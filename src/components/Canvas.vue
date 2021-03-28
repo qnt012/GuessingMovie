a195ec68c2562d919037c31b47b11d36b0aa03f7
20170270 <dgb980228@naver.com>
2021-03-28 21:32:19 +0900

    Update Canvas.vue(apply tensorflow)

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -2,21 +2,42 @@
   <div>
     <div v-show="showSlider" class="slider-div">
       <div class="value">
-        {{brushSize}}
+        {{ brushSize }}
       </div>
-      <input type="range" min="1" max="100" v-model="brushSize" class="slider" id="myRange">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        v-model="brushSize"
+        class="slider"
+        id="myRange"
+      />
     </div>
-    <div
-    class="board-wrapper"
-    :style="boardStyle">
-      <div
-      class="draw-area">
-        <canvas id="canvas" ref="canvas" :width="width" :height="height"></canvas>
-        <canvas id="cursor" ref="cursor" :width="width" :height="height"></canvas>
+    <div class="board-wrapper" :style="boardStyle">
+      <div class="draw-area">
+        <canvas
+          id="canvas"
+          ref="canvas"
+          :width="width"
+          :height="height"
+        ></canvas>
+        <canvas
+          id="cursor"
+          ref="cursor"
+          :width="width"
+          :height="height"
+        ></canvas>
       </div>
       <ul class="tools">
-        <li id="tool-pencil" :class="{ active: selectedToolIdx === 0 }" @click="changeTool(0)">
-          <button class="tools-icon-btn" :class="{ active: selectedToolIdx === 0 }">
+        <li
+          id="tool-pencil"
+          :class="{ active: selectedToolIdx === 0 }"
+          @click="changeTool(0)"
+        >
+          <button
+            class="tools-icon-btn"
+            :class="{ active: selectedToolIdx === 0 }"
+          >
             <i class="fas fa-pencil-alt"></i>
           </button>
         </li>
@@ -40,32 +61,39 @@
             <i class="fas fa-download"></i>
           </button>
         </li>
+        <li id="tool-download" @click="predict">
+          <button class="tools-icon-btn">
+            <i class="fas fa-download"></i>
+          </button>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 <script>
+import * as tf from "@tensorflow/tfjs";
 /* eslint-disable no-return-assign */
 /* eslint-disable no-restricted-globals */
 export default {
   props: {
     width: {
       type: Number,
-      default: 784
+      default: 784,
       //default: screen.width - 100
     },
     height: {
       type: Number,
-      default: 700
+      default: 700,
       //default: screen.height - Number(screen.height) / 100 * 35
     },
     outputName: {
       type: String,
-      default: "canvas"
-    }
+      default: "canvas",
+    },
   },
   data() {
     return {
+      model: null,
       showSlider: false,
       canvasCtx: null,
       cursorCtx: null,
@@ -77,14 +105,14 @@ export default {
       tools: [
         {
           name: "Pencil",
-          color: "#000000"
+          color: "#000000",
         },
         {
-          name: "Eraser"
-        }
+          name: "Eraser",
+        },
       ],
       selectedToolIdx: 0,
-      undoPoints: []
+      undoPoints: [],
     };
   },
   computed: {
@@ -93,15 +121,34 @@ export default {
         color: "red",
         height: `${this.height}px`,
         width: `${this.width + 30}px`,
-        "grid-template-columns": `${this.width}px 50px`
+        "grid-template-columns": `${this.width}px 50px`,
       };
-    }
+    },
   },
-  mounted() {
+  async mounted() {
+    this.model = await tf.loadModel(
+      "https://epfl-exts.github.io/react-course-project/public/model/model.json"
+    );
+    console.log(this.model);
     this.setCanvas();
     this.bindEvents();
   },
   methods: {
+    async predict() {
+      var c = document.getElementById("canvas");
+      const test = await this.model.predict(this.preprocessCanvas(c)).data();
+      console.log(test);
+    },
+    preprocessCanvas(canvas) {
+      let tensor = tf
+        .fromPixels(canvas) // Shape: (300, 300, 3) - RGB image
+        .resizeNearestNeighbor([28, 28]) // Shape: (28, 28, 3) - RGB image
+        .mean(2) // Shape: (28, 28) - grayscale
+        .expandDims(2) // Shape: (28, 28, 1) - network expects 3d values with channels in the last dimension
+        .expandDims() // Shape: (1, 28, 28, 1) - network makes predictions for "batches" of images
+        .toFloat(); // Network works with floating points inputs
+      return tensor.div(255.0); // Normalize [0..255] values into [0..1] range
+    },
     setCanvas() {
       this.canvasCtx = this.$refs.canvas.getContext("2d");
       this.canvasCtx.lineJoin = "round";
@@ -110,7 +157,7 @@ export default {
       this.cursorCtx = this.$refs.cursor.getContext("2d");
     },
     bindEvents() {
-      this.$refs.canvas.addEventListener("mousedown", event => {
+      this.$refs.canvas.addEventListener("mousedown", (event) => {
         this.isDrawing = true;
         [this.lastX, this.lastY] = [event.offsetX, event.offsetY];
       });
@@ -136,7 +183,7 @@ export default {
         lastY: this.lastY,
         brushSize: this.brushSize,
         selectedToolIdx: this.selectedToolIdx,
-        color: this.tools[this.selectedToolIdx].color
+        color: this.tools[this.selectedToolIdx].color,
       };
       this.points.push(point);
       this.redrawAll();
@@ -157,7 +204,7 @@ export default {
     },
     redrawAll() {
       this.canvasCtx.clearRect(0, 0, this.width, this.height);
-      this.points.forEach(point => {
+      this.points.forEach((point) => {
         if (this.tools[point.selectedToolIdx].name === "Eraser") {
           this.canvasCtx.globalCompositeOperation = "destination-out";
         } else {
@@ -190,7 +237,7 @@ export default {
     },
     showColorPalette() {
       const colorPalette = document.createElement("input");
-      colorPalette.addEventListener("change", event => {
+      colorPalette.addEventListener("change", (event) => {
         this.tools[0].color = event.target.value;
       });
       colorPalette.type = "color";
@@ -208,7 +255,7 @@ export default {
       this.undoPoints = [];
       this.changeTool(0);
       this.canvasCtx.clearRect(0, 0, this.width, this.height);
-    }
-  }
+    },
+  },
 };
 </script>
